88611fd87895797ce2225e39f528f321c9b2a9c3
Wilfred Ruck <wilfredoruck@gmail.com>
2019-01-24 16:39:25 -0500

    fix unnecessary colon

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::Messages < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      render: 'app/views/api/channels/show'
+      render 'app/views/api/channels/show'
     else
       render json: @message.errors.full_messages, status: 401
     end
