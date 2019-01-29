9fae59147ad5d862a5f17a307bd042c236b5029d
Wilfred Ruck <wilfredoruck@gmail.com>
2019-01-29 16:28:45 -0500

    update closing of modal

--- frontend/components/channel/channel_nav_menu.jsx
+++ frontend/components/channel/channel_nav_menu.jsx
@@ -5,12 +5,17 @@ import { logoutUser } from '../../actions/session_actions';
 
 const ChannelNavMenu = (props) => {
   return (
-    <div className="nav-modal-background" onClick={closeModal}>
+    <div className="nav-modal-background" onClick={() => props.closeModal()}>
         <div className="nav-modal-child" onClick={e => e.stopPropagation()}>
           <div className="nav-menu-container">  
               <ul className="nav-menu">
-                <li><img src={props.currentUser.avatar} alt="user's avatar" height="36px" width="36px"/>{props.currentUser.username}</li>  
-                <li onClick={() => props.logout()} className="logout-button">Log Out</li>  
+                <li className="user-info">
+                  <div>
+                    <img src={props.currentUser.avatar} alt="user's avatar" height="36px" width="36px"/>
+                    <p>{props.currentUser.username}</p>
+                  </div>
+                </li>  
+                <li onClick={() => {props.logout(); props.closeModal();}} className="logout-button">Log Out</li>  
               </ul>
             </div>
           </div>
