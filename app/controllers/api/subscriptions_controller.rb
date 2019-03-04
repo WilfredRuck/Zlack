c9a274a90ca34460cc39e0d94ea776f0aa4e211e
Wilfred Ruck <wilfredoruck@gmail.com>
2019-03-04 16:23:50 -0500

    take out 422 error from subscription controller

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -1,13 +1,9 @@
 class Api::SubscriptionsController < ApplicationController
   def create
     new_sub = Subscription.new(channel_id: params[:channel_id], user_id: current_user.id)
-    if new_sub.save
-      @channel = Channel.find(params[:channel_id])
-      render "api/channels/show"
-    else
-      render json: new_sub.errors.full_messages, status: 422
-    end
-
+    new_sub.save
+    @channel = Channel.find(params[:channel_id])
+    render "api/channels/show"
   end
 
   def destroy
