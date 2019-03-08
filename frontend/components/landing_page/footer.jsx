6ccde7e28aebfa3ed70b90f8e165afff2b767a54
Wilfred Ruck <wilfredoruck@gmail.com>
2019-03-08 10:09:54 -0500

    added angellist to footer

--- frontend/components/landing_page/footer.jsx
+++ frontend/components/landing_page/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <li><a href="https://www.willthecoder.com" target="_blank" className="portfolio">Portfolio</a></li>
         <li><a href="https://www.github.com/wilfredruck" target="_blank" className="github">Github</a></li>
         <li><a href="https://www.linkedin.com/in/wruck" target="_blank" className="linkedin">LinkedIn</a></li>
-        <li><a href="https://www.instagram.com/willthefred" target="_blank" className="instagram">Instagram</a></li>
+        <li><a href="https://angel.co/wilfred-ruck" target="_blank" className="angellist">AngelList</a></li>
       </ul>
     </div>
   )
